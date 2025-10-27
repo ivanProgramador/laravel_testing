7a3e2c6cc84ce4b6bf37ca7207dda1da4bf17eac
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-10-27 15:55:48 -0300

    sobre expectation api

--- tests/Unit/ExpectationApiTest.php
+++ tests/Unit/ExpectationApiTest.php
@@ -1,7 +1,7 @@
 <?php
 
 describe('Testando expectation api', function () {
-    it('Testeando um função ', function () {
+    it('Testando uma função ', function () {
          
         $valor = 10; 
 
@@ -14,7 +14,7 @@ describe('Testando expectation api', function () {
 
     });
 
-    it('Testando se o vaor e falso ou verdadeiro', function () {
+    it('Testando se o valor e falso ou verdadeiro', function () {
         $valor1 = true;
         $valor2 = false; 
 
@@ -42,6 +42,32 @@ describe('Testando expectation api', function () {
         expect($valor1)->toBeArray();
         
     });
+
+     it('Testando se um valor existe dentro de uma coleção de valores ', function () {
+        
+        //valor que sera pesaquisado  
+        $valor =10;
+        
+        //lista que sera testada 
+        $valores =[10,20,30];
+        
+        //se o valor 10 existior dentro do array de valores ele vai 
+        //retronar  PASS caso não ele retorna Error 
+
+        expect($valor)->toBeIn($valores);
+     });
+
+     it('Testando se o valor devolvido é um json valido',function(){
+          $valor = '{"nome":"Jose"}';
+
+          expect($valor)->toBeJson();
+     });
+
+       it('Testando se uma expressão regular existe dentro de uma string',function(){
+          $valor = "olá mundo";
+
+          expect($valor)->toMatch("/mundo/");
+     });
 
 
 });
